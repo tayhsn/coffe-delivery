a85970be8029eca87c879cf81976be3494cad270
Tayanne <tayh.snovaes@gmail.com>
2022-10-18 13:56:13 -0300

    fix: bg style

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,5 +4,5 @@ import homeBg from '../../assets/home-bg.png'
 export const HomeContainer = styled.div`
   width: 100%;
 
-  background: url(${homeBg}) no-repeat cover;
+  background: url(${homeBg}) no-repeat;
 `
